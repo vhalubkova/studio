c06fb4bb437e5f588864b3893fd105b2c3410d39
vhalubkova <volha-halubkova@yandex.com>
2022-12-07 12:52:26 +0300

    optimize imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,5 @@
 package org.example;
 
-import org.example.model.Artist;
-import org.example.model.Genre;
-
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
 
